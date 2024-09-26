4d895955fb413f1382e339fe4e6b05e88de66be0
Andrea O'Higgins <u20221b178@upc.edu.pe>
2024-09-25 20:29:27 -0500

    add post activity

---
+++ app/src/main/java/com/example/mobileapp_plusroom/PostActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mobileapp_plusroom
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_post)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val viewAuthorProfile: Button = findViewById(R.id.btViewAuthorProfile)
+
+//        viewAuthorProfile.setOnClickListener {
+//            val intent = Intent(this, AuthorProfileActivity::class.java)
+//            startActivity(intent)
+//        }
+
+    }
+}
