eb30dc77024f7a91845c4e145ddbb31c67d3fe9a
Gerald <gpserrano.18.4@gmail.com>
2024-09-26 16:27:15 -0500

    feat: add Customer and Post lists

--- app/src/main/java/com/example/mobileapp_plusroom/CustomerActivity.kt
+++ app/src/main/java/com/example/mobileapp_plusroom/CustomerActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mobileapp_plusroom
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CustomerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,21 @@ class CustomerActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val currentActivity = this.javaClass
+            val targetActivity = when (item.itemId) {
+                R.id.bottom_home -> WelcomeActivity::class.java
+                R.id.bottom_profile -> ProfileActivity::class.java
+                else -> return@setOnItemSelectedListener false
+            }
+
+            if (currentActivity != targetActivity) {
+                startActivity(Intent(this, targetActivity))
+            }
+            true
         }
     }
 }
